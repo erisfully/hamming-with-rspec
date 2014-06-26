bc8c2137e246e4f19df5670efb17101cd912e174
Ashley <a.clarkem@gmail.com>
2014-06-26 09:32:46 -0600

    finished hamming exercise

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,27 @@
 class DnaStrandPair
+  def initialize (strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+
+  end
+
+  def hamming_distance
+    first = @strand_1.split(//)
+    second = @strand_2.split(//)
+
+    x=0
+    y=0
+
+    while y < first.count && y < second.count
+      if first[y] == second[y]
+        x += 0
+      else
+        x += 1
+      end
+      y += 1
+    end
+    x
+
+  end
 
 end
